2cb18b084c2b4f74ccf3164884f96904d0c2694a
Akbar <akbarkhawaja321@gmail.com>
2024-05-16 21:12:27 -0700

    Added 227 Doc

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,21 +5,22 @@ import { NextResponse } from 'next/server';
 export async function GET() {
 	async function modifyPdf() {
 		try {
-			const filePath = "/Users/akbarkhawaja//Dev/PDF-Auto-Populator/public/pdfs/dmv262.pdf"
+			const filePath = "/Users/akbarkhawaja//Dev/PDF-Auto-Populator/public/pdfs/Reg227.pdf"
 			const existingPdfBytes = await fs.readFile(filePath);
-		
-			const pdfDoc = await PDFDocument.load(existingPdfBytes);
+	
+			const pdfDoc = await PDFDocument.load(existingPdfBytes, { ignoreEncryption: true });
 			
-
+			
 			const pdfBytes = await pdfDoc.save();
             const base64Pdf = Buffer.from(pdfBytes).toString('base64');
 
             return NextResponse.json({ pdfData: base64Pdf });
-
+                   
 		} 
 		catch (error) {
-			console.error("Error modifying PDF:", error);
-		}
+            console.error("Error modifying PDF:", error);
+           
+        }
 	}
 	return modifyPdf()
 }
