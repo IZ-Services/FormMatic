276a96117b7f68ff06879e75eb62f136924aa564
Zain Jaffri <zainjaffri2@gmail.com>
2024-05-23 19:17:11 -0700

    Feat: Added new inputs on form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -155,10 +155,29 @@ export default function Home() {
           Residence Or Business Address
         </h3>
 
-        <label style={{ marginLeft: "10px" }}>Mailing Address</label>
+        <label style={{ marginLeft: "10px" }}>Residence/Business Address</label>
         <input className="inputData"></input>
 
         <label style={{ marginLeft: "10px", marginTop: "10px" }}>APT/SPACE/STE#</label>
+        <input className="inputData"></input>
+
+        <label style={{ marginLeft: "10px", marginTop: "10px" }}>City</label>
+        <input className="inputData"></input>
+
+        <label style={{ marginLeft: "10px", marginTop: "10px" }}>State</label>
+        <input className="inputData"></input>
+
+        <label style={{ marginLeft: "10px", marginTop: "10px" }}>Zip Code</label>
+        <input className="inputData"></input>
+
+        <h3 className="title">
+          Mailing Address
+        </h3>
+
+        <label style={{ marginLeft: "10px" }}>Mailing   Address</label>
+        <input className="inputData"></input>
+
+        <label style={{ marginLeft: "10px", marginTop: "10px" }}>PO Box</label>
         <input className="inputData"></input>
 
         <label style={{ marginLeft: "10px", marginTop: "10px" }}>City</label>
@@ -183,14 +202,38 @@ export default function Home() {
         <label style={{ marginLeft: "10px", marginTop: "10px" }}>Make</label>
         <input className="inputData"></input>
 
-        <label style={{ marginLeft: "10px", marginTop: "10px" }}>State</label>
-        <input className="inputData"></input>
+        <label style={{ marginLeft: "10px", marginTop: "10px" }}>Seller Date: Month</label>
+        <input className="smallInputData"></input>
+        
+        <label style={{ marginLeft: "10px", marginTop: "10px" }}>Seller Date: Day</label>
+        <input className="smallInputData"></input>
 
-        <label style={{ marginLeft: "10px", marginTop: "10px" }}>Zip Code</label>
+        <label style={{ marginLeft: "10px", marginTop: "10px" }}>Seller Date: Year</label>
+        <input className="smallInputData"></input>
+
+        <label style={{ marginLeft: "10px", marginTop: "10px" }}>Purchase Price</label>
         <input className="inputData"></input>
       </div>
-
-
+      
+      <div style={{marginTop: "15px"}}>
+        <label>
+          <input
+            type="radio"
+            value="Gift"
+            name="GiftOrTrade"
+          />
+          GIFT
+        </label>
+              
+        <label>
+          <input
+            type="radio"
+            value="Trade"
+            name="GiftOrTrade"
+          />
+          TRADE
+        </label>      
+      </div>
 
       <div className="bottomContainer">
         <button className="buttonNewCustomer">Save</button>
---
+++ src/context/index.tsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+export function AppWrapper({ children } : {
+    children: React.ReactNode;
+}) {
+    
+}
