cfd6dc91a2209eec075d319d049c6a1e56016a0e
Zain Jaffri <zainjaffri2@gmail.com>
2024-06-13 11:04:51 -0700

    feat: Forgot password page

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+import React from 'react';
+import { useState } from 'react';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import app from '../../../firebase-config'
+import "../globals.css";
+
+export default function forgotPassword() {
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState({text: "", type: "success"});
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        const auth = getAuth(app);
+
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                setMessage({text: "Check your email for the password reset link.", type: "success"});
+            })
+            .catch((error) => {
+                setMessage({text: "Failed to send password reset email. Ensure the email is correct and try again later.", type: "error"});
+                console.error("Error sending password reset email:", error);
+            });
+    };
+
+    return (
+        <div className="center-container">
+            <div>
+                <h1 className="login-CompanyName">FormMatic</h1>
+                <p className="login-companySlogan">From Data to Documents in Seconds.</p>
+                <h2 className="forgotPasswordTitle">Forgot Password?</h2>
+                
+                <input
+                    className='usernameInput'
+                    type="text"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                ></input>
+
+                {message.text && (
+                    <p className={`error-message-login ${message.type === 'error' ? 'text-error' : 'text-success'}`}>
+                        {message.text}
+                    </p>
+                )}
+
+                <button className="loginButton" onClick={handleSubmit}>
+                    Submit
+                </button>
+
+                <div className="forgot-password-container">
+                    <a href="/login" className="forgot-password-link">Return to Sign In</a>
+                </div>
+            </div>
+        </div>
+    );
+}
