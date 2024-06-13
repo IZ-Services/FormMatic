108828cb612124ab5cee93d7fba795d457194a7c
Akbar <akbarkhawaja321@gmail.com>
2024-06-13 12:10:04 -0700

    Prettier and linting

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,27 +1,26 @@
-"use client";
-import React from "react";
-import { PencilSquareIcon } from "@heroicons/react/24/outline";
+'use client';
+import React from 'react';
+import { PencilSquareIcon } from '@heroicons/react/24/outline';
 import '../globals.css';
 
-export default function Account(){
-
-	return (
-		<div>
-			<h1>Account</h1>
-			<div className="accountInputWrapper">
-				<div>
-					<button className="accountButtons">Account Settings</button>
-					<button className="accountButtons">Payment Settings</button>
-				</div>
-				<input type="text" placeholder="Company"/>
-				<input type="text" placeholder="Email Address"/>
-				<div className="inputWithIcon">
-					<input type="password" placeholder="Password" />
-					<PencilSquareIcon className="icon" />
-				</div>
-				<input type="text" placeholder="Confirm Password"/>
-				<button>Save</button>
-			</div>
-		</div>
-	);
+export default function Account() {
+  return (
+    <div>
+      <h1>Account</h1>
+      <div className="accountInputWrapper">
+        <div>
+          <button className="accountButtons">Account Settings</button>
+          <button className="accountButtons">Payment Settings</button>
+        </div>
+        <input type="text" placeholder="Company" />
+        <input type="text" placeholder="Email Address" />
+        <div className="inputWithIcon">
+          <input type="password" placeholder="Password" />
+          <PencilSquareIcon className="icon" />
+        </div>
+        <input type="text" placeholder="Confirm Password" />
+        <button>Save</button>
+      </div>
+    </div>
+  );
 }
