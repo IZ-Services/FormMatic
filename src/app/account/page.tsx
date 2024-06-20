ff2a7d22f719131e1319e13e3aa9f88bdbeb7125
Zain Jaffri <zainjaffri2@gmail.com>
2024-06-20 08:59:43 -0700

    feat: Added stuff

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,25 +1,54 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { PencilSquareIcon } from '@heroicons/react/24/outline';
 import '../globals.css';
 
 export default function Account() {
+  const [accountSettingsClicked, setAccountSettingsClicked] = useState(false)
+  const [paymentSettingsClicked, setPaymentSettingsClicked] = useState(false)
+
+  const handleAccountSettingsClick = () => {
+    setAccountSettingsClicked(true);
+    setPaymentSettingsClicked(false);
+  }
+
+  const handlePaymemtSettingsClick = () => {
+    setPaymentSettingsClicked(true);
+    setAccountSettingsClicked(false);
+  }
+
   return (
-    <div>
-      <h1>Account</h1>
-      <div className="accountInputWrapper">
-        <div>
-          <button className="accountButtons">Account Settings</button>
-          <button className="accountButtons">Payment Settings</button>
-        </div>
-        <input type="text" placeholder="Company" />
-        <input type="text" placeholder="Email Address" />
-        <div className="inputWithIcon">
-          <input type="password" placeholder="Password" />
-          <PencilSquareIcon className="icon" />
-        </div>
-        <input type="text" placeholder="Confirm Password" />
-        <button>Save</button>
+    <div className="center-container">
+      <div>
+        <h1 className="login-SignIn">Account</h1>
+          <button className="accountButtonsAccountSettings">Account Settings</button>
+          <button className="accountButtonsPaymentSettings">Payment Settings</button>
+
+          <div style={{marginTop: "75px"}}>
+          <input
+            className="usernameInput"
+            type="text"
+            placeholder="Company"
+          ></input>
+          
+          <input
+            className="usernameInput"
+            type="text"
+            placeholder="Email"
+          ></input>
+
+          <div className="input-container-forIcon">
+            <input
+              className="passwordInput"
+              type="text"
+              placeholder="Password"
+            />
+            <button className="toggle-password">
+              <PencilSquareIcon className="input-icon" />
+            </button>
+          </div>
+          
+          </div>
       </div>
     </div>
   );
