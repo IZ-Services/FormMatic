0051b12b45c684a8b39a97038f11c7b1582342ba
Akbar <akbarkhawaja321@gmail.com>
2024-06-27 11:19:11 -0700

    Fix contact page plural issue

--- src/app/contactUs/page.tsx
+++ src/app/contactUs/page.tsx
@@ -20,7 +20,7 @@ export default function Contact() {
       <h1 className="contactHeading">We&apos;d love to hear from you!</h1>
       <div className="content">
         <p className="contactMessage">
-          At Formatics, we prioritize our customers&apos; experience. Our goal is to provide
+          At Formatic, we prioritize our customers&apos; experience. Our goal is to provide
           software that not only enhances your business efficiency but also makes your life easier.
           We value your feedback and constructive criticism. If there is anything we can do to
           improve your user experience, or if you would simply like to say hello, please feel free
@@ -28,13 +28,13 @@ export default function Contact() {
           evolve. Whether you have a suggestion for a new feature, encountered a bug, or have a
           question about how to get the most out of our software, our dedicated support team is here
           to assist you. Your satisfaction is our top priority, and we strive to respond to all
-          inquiries promptly and thoroughly. Thank you for choosing Formatics.
+          inquiries promptly and thoroughly. Thank you for choosing Formatic.
         </p>
         <div className="contactInfo">
           <div className="outerContact">
             <div className="contactItem">
               <EnvelopeIcon className="icon" />
-              <span className="contactText">Formatics@gmail.com</span>
+              <span className="contactText">Formatic@gmail.com</span>
             </div>
             <div className="contactItem">
               <PhoneIcon className="icon" />
