ab11991be7415e4e7371eec18f71d383d3cd815a
Akbar <akbarkhawaja321@gmail.com>
2024-06-27 11:19:58 -0700

    ESLint

--- src/app/contactUs/page.tsx
+++ src/app/contactUs/page.tsx
@@ -20,14 +20,14 @@ export default function Contact() {
       <h1 className="contactHeading">We&apos;d love to hear from you!</h1>
       <div className="content">
         <p className="contactMessage">
-          At Formatic, we prioritize our customers&apos; experience. Our goal is to provide
-          software that not only enhances your business efficiency but also makes your life easier.
-          We value your feedback and constructive criticism. If there is anything we can do to
-          improve your user experience, or if you would simply like to say hello, please feel free
-          to reach out. We believe in continuous improvement and your insights help us to constantly
-          evolve. Whether you have a suggestion for a new feature, encountered a bug, or have a
-          question about how to get the most out of our software, our dedicated support team is here
-          to assist you. Your satisfaction is our top priority, and we strive to respond to all
+          At Formatic, we prioritize our customers&apos; experience. Our goal is to provide software
+          that not only enhances your business efficiency but also makes your life easier. We value
+          your feedback and constructive criticism. If there is anything we can do to improve your
+          user experience, or if you would simply like to say hello, please feel free to reach out.
+          We believe in continuous improvement and your insights help us to constantly evolve.
+          Whether you have a suggestion for a new feature, encountered a bug, or have a question
+          about how to get the most out of our software, our dedicated support team is here to
+          assist you. Your satisfaction is our top priority, and we strive to respond to all
           inquiries promptly and thoroughly. Thank you for choosing Formatic.
         </p>
         <div className="contactInfo">
