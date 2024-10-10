d8050eba90149de1000713647c4f75db1f9cde42
Akbar <akbarkhawaja321@gmail.com>
2024-10-10 15:16:41 -0700

    Functions session

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,28 @@
-import * as v2 from 'firebase-functions/v2';
+import {onRequest} from "firebase-functions/v2/https";
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
 
-export const managesessions = v2.https.onCre((request, response) => {});
+initializeApp();
+const db = getFirestore();
+
+export const manageUserSessions = onRequest(async (req, res) => {
+  const {userId, authTime} = req.body;
+  const sessionId = Math.random().toString(36).substring(2);
+  const userSessionsRef = db.collection("users").doc(userId)
+    .collection("sessions");
+  const sessionsSnapshot = await userSessionsRef
+    .orderBy("authTime", "asc")
+    .get();
+
+  if (sessionsSnapshot.size >= 2) {
+    const oldestSession = sessionsSnapshot.docs[0];
+    await oldestSession.ref.delete();
+  }
+
+  await userSessionsRef.doc(sessionId).set({
+    deviceId: sessionId,
+    authTime,
+  });
+
+  res.send({sessionId});
+});
